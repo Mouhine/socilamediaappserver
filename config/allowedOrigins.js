9934acdf734ad44695da917ee9058d44fcbd52a1
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-02-16 15:15:03 +0000

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5000",
   "http://localhost:3000",
+  "https://socialmediaapp-psi.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
