852e441c03a3c0022ebeb9e28d3c2aa34da396ad
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-02-21 13:06:50 +0000

    ff

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://socialmediaapp-jet.vercel.app/",
   "https://socialmediaapp-git-main-mouhine.vercel.app/",
+  "https://socialmediaapp-git-main-mouhine.vercel.app",
 ];
 
 module.exports = allowedOrigins;
