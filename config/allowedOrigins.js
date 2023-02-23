3ab28802fd5fe4046d77364ac651f2eed2a79391
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-02-23 10:38:33 +0000

    netlify

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,8 @@ const allowedOrigins = [
   "https://socialmediaapp-git-main-mouhine.vercel.app/",
   "https://socialmediaapp-git-main-mouhine.vercel.app",
   "https://socialmediaapp-goll.vercel.app",
+  "https://main--shiny-sfogliatella-3a3566.netlify.app",
+  "https://main--shiny-sfogliatella-3a3566.netlify.app/",
 ];
 
 module.exports = allowedOrigins;
